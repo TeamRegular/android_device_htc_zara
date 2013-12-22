32fcabc0245eeaa70f7b04fbc14d4352d8d008f7
C457 <android.c357@gmail.com>
2013-12-21 20:33:06 -0600

    Add cpu_variant for 4.4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,6 +3,7 @@ USE_CAMERA_STUB := true
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT  := cortex-a9
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
